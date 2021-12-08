925e9c96976bccce4fa2ca789f2c2f4a731e7e8f
Quinton Pedrick <77907943+qpedrick@users.noreply.github.com>
2021-12-08 13:40:29 -0500

    added quinton branch

--- src/components/API/Nasa.js
+++ src/components/API/Nasa.js
@@ -1,3 +1,5 @@
+const key = '9OyDJd4K3mQV7jlaO0tLvP1Gsibafxns1tMSrzab'
+
 const Nasa = () => {
     return(
         <div className = 'main'>
@@ -7,5 +9,41 @@ const Nasa = () => {
         </div>
     )
 };
+
+/*
+
+const Hooks2 = () => {
+    const [results, queryNum, setQueryNum] = useNumHook('')
+
+    return(
+        <div className = 'main'>
+            <div className = 'mainDiv'>
+                <h3>Enter a number below to see a number fact</h3>
+                <input value = {queryNum} onChange={e => setQueryNum(e.target.value)} placeholder='enter a number' />
+                {results ? <h2>{results}</h2> : <div></div>}
+            </div>
+        </div>
+    )
+}
+
+const useNumHook = (num) => {
+    const [queryNum, setQueryNum] = useState(num);
+    const [results, setResults] = useState('');
+
+    useEffect(() => {
+        if (queryNum !== ''){
+            fetch(`http://numbersapi.com/${queryNum}`)
+            .then(res=>res.text())
+            .then(json => {
+                setResults(json);
+                console.log(json)
+            })
+        }
+    }, [queryNum])
+
+    return [results, queryNum, setQueryNum]
+}
+
+*/
 
 export default Nasa;
