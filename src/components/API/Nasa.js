b8baf0fc73854a834e115a090c3d55ad24f433e1
Kiersten Gamby <kierstengamby@gmail.com>
2021-12-09 09:51:22 -0500

    mui added and some additional styling to nasa.js

--- src/components/API/Nasa.js
+++ src/components/API/Nasa.js
@@ -1,6 +1,8 @@
 /*API key = '9OyDJd4K3mQV7jlaO0tLvP1Gsibafxns1tMSrzab'*/
 
-import React, {useState} from "react";
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 
 const Nasa = () => {
     const [lat, setLat] = useState(null);
@@ -9,7 +11,7 @@ const Nasa = () => {
     const [image, setImage] = useState(null);
     const key = '9OyDJd4K3mQV7jlaO0tLvP1Gsibafxns1tMSrzab';
     const date = '2020-01-01';
-    
+
     const getLocation = () => {
         if (!navigator.geolocation) {
             setStatus('Geolocation is not supported by your broswer');
@@ -27,30 +29,30 @@ const Nasa = () => {
 
     const getImage = () => {
         fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${lng}&lat=${lat}&date=${date}&api_key=${key}`)
-        .then(res => res.json())
-        .then(json => {
-            setImage(json.url);
-            console.log(json.url)
-        })
-        .catch(err => console.log(err));
+            .then(res => res.json())
+            .then(json => {
+                setImage(json.url);
+                console.log(json.url)
+            })
+            .catch(err => console.log(err));
     }
-    
 
-    return(
-        <div className = 'main'>
-            <div className = 'mainDiv'>
-            <h1>Nasa API</h1>
-            <button onClick = {getLocation}>GetLocation</button>
+
+    return (
+        <div className='main'>
+            <div className='mainDiv'>
+                <h1>Nasa API</h1>
+                <Button onClick={getLocation} variant="contained">GetLocation</Button>
                 <h2>Coordinates</h2>
                 <p>{status}</p>
                 {lat && <p>Latitude: {lat}</p>}
                 {lng && <p>Longitude: {lng}</p>}
                 <h3>Image</h3>
-                <button onClick={() => getImage()}>Get Image Console</button>
+                <Button onClick={() => getImage()} variant="contained">Get Image Console</Button>
                 <div>
-                <img alt = 'Please get location first :)' src = {image} height = '250px' width = '250px'></img>
+                    <img alt='Please get location first :)' src={image} height='250px' width='250px'></img>
                 </div>
-        </div>
+            </div>
         </div>
     )
 };
