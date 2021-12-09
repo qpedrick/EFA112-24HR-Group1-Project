7a01c81fbd6a9ddea25e984cb2837b164579f160
Andrew Wesson <andrewtwesson@gmail.com>
2021-12-09 10:38:02 -0500

    i will cry if no work

--- src/components/API/OpenWeather.js
+++ src/components/API/OpenWeather.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react"
-
+import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
 const OpenWeather = () => {
     const [lat, setLat] = useState(null);
     const [lng, setLng] = useState(null);;
     const [results, setResults] = useState(null);
     const key = `08d029604b596f179b40bb36b23a43c3`;
     //const altkey = '87fdda7a943f4b425d67f817af9cab46'
-
     const getLocation = () => {
         if (!navigator.geolocation) {
             console.log(`Geolocation is not supported by your broswer`);
@@ -23,16 +23,18 @@ const OpenWeather = () => {
         }
     }
     const fetchCurrentWeather = () => {
-        getLocation();
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}}&lon=${lng}&appid=${key}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`)
             .then(res => res.json())
-            .then(data => setResults(data.main))
-    }
+            .then(data => {
+                setResults(data.main)
+            })
+            .catch(err => console.log(err))
+        }
     return (
         <div className='main'>
+            {getLocation()}
             <div className='mainDiv'></div>
             <h1>OpenWeather API</h1>
-
             {results ? <table>
                 <thead>
                     <tr>
@@ -47,7 +49,7 @@ const OpenWeather = () => {
                 <td>{`${Math.round(results.temp_min - 273.15)}°C`}</td>
                 <td>{`${Math.round(results.temp_max - 273.15)}°C`}</td>
             </table> : <div></div>}
-            <button onClick={fetchCurrentWeather}>Find weather</button>
+            <Button onClick={fetchCurrentWeather} variant="contained">Find Weather</Button>
         </div>
     )
 }
