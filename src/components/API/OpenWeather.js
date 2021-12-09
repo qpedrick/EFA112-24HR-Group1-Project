aa6dcd6a7cd911ea79dd8d82140090ba915d3ecf
Andrew Wesson <andrewtwesson@gmail.com>
2021-12-08 21:22:00 -0500

    finished fetch working on table

--- src/components/API/OpenWeather.js
+++ src/components/API/OpenWeather.js
@@ -1,65 +1,65 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 
-
-
-
-const OpenWeather = () => {
+const OpenWeather = props => {
+    const [weather, setWeather] = useState({
+    });
     const [lat, setLat] = useState(null);
     const [lng, setLng] = useState(null);
-    const [status, setStatus] = useState(null);
-    const key = `37f21f442b6bb2448534b4c2e267c937`;
-    const date = `2014-02-01`;
+    const key = `08d029604b596f179b40bb36b23a43c3`;
     const getLocation = () => {
         if (!navigator.geolocation) {
-            setStatus(`Geolocation is not supported by your broswer`);
+            console.log(`Geolocation is not supported by your broswer`);
         } else {
-            setStatus(`Locating...`);
+            console.log(`Locating...`);
             navigator.geolocation.getCurrentPosition((position) => {
-                setStatus(null);
                 setLat(position.coords.latitude);
+                console.log(lat);
                 setLng(position.coords.longitude);
+                console.log(lng);
             }, () => {
-                setStatus(`Unable to retrieve your location`);
+                console.log(`Unable to retrieve your location`);
             });
         }
     }
     const fetchCurrentWeather = () => {
-
-        fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`)
+        getLocation();
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${25}&lon=${85}&appid=${key}`)
             .then(res => res.json())
-            .then(data => {
-                console.log(data)
+            .then(json => {
+                console.log(json);
+                setWeather(json)
             })
-    }
-
-    /*const fetchCurrentWeather = () => {
-        let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${key}`
-
-        fetch(url, {
-            method: "GET",
-            headers: new Headers({
-                "Content-Type": "application/json",
-
-            })
-        })
-            .then(res => res.json())
-            .then(data => setWeather(data))
             .catch(err => console.log(err))
     }
-    useEffect(() => {
-        fetchCurrentWeather()
-        console.log(weather);
-    })*/
 
 
     return (
         <div className='main'>
             <div className='mainDiv'></div>
-            <h1>OpenWeather API</h1>
-            <p>Build out project here</p>
-            <button onClick={fetchCurrentWeather}>fetch</button>
-        </div>
-    )
 
+            <button onClick={fetchCurrentWeather()}>Get Weather near me.</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Sky</th>
+                        <th>Temp</th>
+                        <th>Feels like</th>
+                        <th>Temp Min</th>
+                        <th>Temp Max</th>
+                        <th>WindSpeed</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>{weather.main.temp}</td>
+                    <td>{weather.main.temp}</td>
+                    <td>{weather.main.temp}</td>
+                    <td>{weather.main.temp}</td>
+                    <td>{weather.main.temp}</td>
+                    <td>{weather.main.temp}</td>
+                </tr>
+            </table>
+        </div >
+    )
 }
+
 export default OpenWeather;
